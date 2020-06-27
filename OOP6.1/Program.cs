eca73c78b7e5e4ce969fa964ca965d40b44023cb
POSTOVALOVDENIS <67200908+POSTOVALOVDENIS@users.noreply.github.com>
2020-06-28 00:12:19 +0500

    Добавьте файлы проекта.

---
+++ OOP6.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace OOP6._1
+{
+    class num
+    {
+        private int odin;
+        private int dva;
+        public num()
+        {
+        }
+        public num(int pervoe)
+
+        {
+            odin = pervoe;
+        }
+        public num(int pervoe, int vtoroe)
+        {
+            odin = pervoe;
+            dva = vtoroe;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"{odin} {dva}");
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            num Numbers = new num();
+            Numbers.Display();
+            num Num1 = new num(666);
+            Num1.Display();
+            num Num2 = new num(777, 777);
+            Num2.Display();
+            
+            Console.ReadLine();
+         
+        }
+    }
+}
